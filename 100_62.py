bf8f78e1f28477945d225e90bf06d4e667509998
Zehan Tian <45849694+Silky-Littleraisin@users.noreply.github.com>
2019-01-02 23:09:31 +0800

    Update 100_62.py

--- 100_62.py
+++ 100_62.py
@@ -9,6 +9,7 @@ fname_db = 'test_db'
 # LevelDBオープン
 db = leveldb.DB(bytes("/Users/silky/Documents/GitHub/Practice/level_test", "ascii"),create_if_missing=True)
 result=[]
+
 # valueが'Japan'のものを列挙
 clue = 'Japan'.encode()
 #result = [key.decode() for key,value in db if value == clue]
