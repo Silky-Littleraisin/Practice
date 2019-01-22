0d611318af7d9cb1936bf828b711e8f2bc316868
Zehan Tian <45849694+Silky-Littleraisin@users.noreply.github.com>
2019-01-22 16:12:52 +0800

    1.22

--- 100_53.py
+++ 100_53.py
@@ -9,7 +9,7 @@ def parse_nlp():
 
     '''nlp.txtをStanford Core NLPで解析しxmlファイルへ出力
     すでに結果ファイルが存在する場合は実行しない    '''
-    if not os.path.exists(fname_parsed):
+    #if not os.path.exists(fname_parsed):
 
         # StanfordCoreNLP実行、標準エラーはparse.outへ出力
 
