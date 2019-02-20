a19d7ea35091545d230023335a6abb886c2563cf
Zehan Tian <45849694+Silky-Littleraisin@users.noreply.github.com>
2019-02-20 13:24:43 +0800

    2.20

--- 100_63.py
+++ 100_63.py
@@ -33,7 +33,7 @@ with gzip.open(fname, 'rt') as data_file:
             db.put(key.encode(), json.dumps(value).encode())
 
     # 確認のため登録件数を表示
-print('{}件登録しました。'.format(len(list(db.keys()))))
+#print('{}件登録しました。'.format(len(list(db.keys()))))
 
 #except:
   #  db = leveldb.DB(bytes("/Users/silky/Documents/GitHub/Practice/level_test", "ascii"),create_if_missing=True)
