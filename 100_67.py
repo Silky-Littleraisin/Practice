7d344a054fb04ea8dcaab87b40f7182bd535832d
Zehan Tian <45849694+Silky-Littleraisin@users.noreply.github.com>
2019-04-05 23:21:43 +0800

    4.5

--- 100_67.py
+++ 100_67.py
@@ -6,7 +6,6 @@ import pymongo
 
 fname = 'artist.json.gz'
 fname_db = 'test_db'
-
 client=pymongo.MongoClient(host='localhost', port=27017)
 
 mongo_db=client.mongo
@@ -14,7 +13,7 @@ mongo_db.collection1
 collection=mongo_db.collection1
 
 
-with gzip.open(fname, 'rt') as data_file:
+#with gzip.open(fname, 'rt') as data_file:
     for line in data_file:
         data_json = json.loads(line)
         #每个data_file如果list内值多于一就复制值的数量的分身，然后每个分配一个list使其成为单值
