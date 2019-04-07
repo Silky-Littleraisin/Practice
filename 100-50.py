cfc45a436d8b1fe72339c6d5c0d33df87deccd13
Zehan Tian <45849694+Silky-Littleraisin@users.noreply.github.com>
2019-04-07 19:36:57 +0800

    4.7

--- 100-50.py
+++ 100-50.py
@@ -27,7 +27,7 @@ def nlp_lines():
             while len(line) > 0:
 
                 match= pattern.match(line)
-                if match:
+                #if match:
 
                     yield match.group(1)
                     line=match.group(2)
