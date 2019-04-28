e062cd1ece227de13e1f4372918637012cff5f96
Zehan Tian <45849694+Silky-Littleraisin@users.noreply.github.com>
2019-04-28 15:45:20 +0800

    4.28

--- 100_64.py
+++ 100_64.py
@@ -8,7 +8,7 @@ fname = 'artist.json.gz'
 fname_db = 'test_db'
 client=pymongo.MongoClient(host='localhost', port=27017)
 
-#mongo_db=client.mongo
+mongo_db=client.mongo
 mongo_db.collection1
 collection=mongo_db.collection1
 
